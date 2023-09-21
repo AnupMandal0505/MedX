4c3e24dc74617c24d1c9ebf13ec7107465ad0fb9
anup <anupmandal828109@gmail.com>
2023-09-22 01:44:39 +0530

    remove

--- HACKATHON/hmsproject/app/static/dasboard/js/emailvalid.js
+++
@@ -1,8 +0,0 @@
-console.log('I am connected!!')
-let email;
-var emailfield=document.querySelector('#email');
-// console.log(emailfield);
-document.querySelector('#email').addEventListener("change", getValue);
-const getValue=(e)=>{
-    console.log(e.target.value);
-}
