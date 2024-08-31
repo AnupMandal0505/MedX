5ae2965981d216b665bf80b9daf88be4844d140d
ANUP MANDAL <117968847+AnupMandal0505@users.noreply.github.com>
2024-08-31 19:39:54 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -140,8 +140,8 @@ STATIC_ROOT = BASE_DIR / 'collectstatic'
 # smtp email send
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mastikipathshala828109@gmail.com'
-EMAIL_HOST_PASSWORD = 'rbrrafaesffcjkrw'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = True
 
 # Store Image Folder name Media
